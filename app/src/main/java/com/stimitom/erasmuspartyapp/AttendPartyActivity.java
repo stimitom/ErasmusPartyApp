f43ac8d8b7a880ad5c8f6a41a2e3da6afbb1dc47
tstim <e1551905@student.tuwien.ac.at>
2019-10-01 19:20:20 +0300

    update of User in database not working, but doesnt break tha shit

--- app/src/main/java/com/stimitom/erasmuspartyapp/AttendPartyActivity.java
+++ app/src/main/java/com/stimitom/erasmuspartyapp/AttendPartyActivity.java
@@ -1,18 +1,29 @@
 package com.stimitom.erasmuspartyapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AttendPartyActivity extends AppCompatActivity {
 
@@ -22,6 +33,9 @@ public class AttendPartyActivity extends AppCompatActivity {
     private TextView numberOfAttendeesView;
     private Button attendButton;
     private Boolean clicked = false;
+    private FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    private final String TAG = "AttendPartyActivity";
+    DocumentReference userRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +55,12 @@ public class AttendPartyActivity extends AppCompatActivity {
         venuePicture.setImageResource(venue.getImageId());
         venueRating.setText(venue.getRating());
         numberOfAttendeesView.setText(Integer.toString(venue.getNumberOfAttendees()));
-
+        if (user != null){
+             userRef = db.collection("users")
+                    .document(getUserId());
+        }else{
+            //TODO send to login
+        }
 
         attendButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,6 +72,7 @@ public class AttendPartyActivity extends AppCompatActivity {
                     attendButton.setText(R.string.dontgo);
                     attendButton.setBackgroundColor(Color.RED);
                     updateNumberOfAttendees(venue_name, updatedCount);
+                    addToListOfVenuesAttended(venue);
                     clicked = true;
                 } else {
                     venue.decreaseNumberOfAttendees();
@@ -61,22 +81,79 @@ public class AttendPartyActivity extends AppCompatActivity {
                     attendButton.setText(R.string.attend);
                     attendButton.setBackgroundColor(Color.GREEN);
                     updateNumberOfAttendees(venue_name, updatedCount);
+                    deleteFromListOfVenuesAttended(venue);
                     clicked = false;
                 }
             }
         });
-
     }
+
+
 
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private CollectionReference venuesRef = db.collection("venues");
-    //private DocumentReference currentUserRef = db.collection("users");
+
 
     public void updateNumberOfAttendees(String venueName, int count) {
         DocumentReference venueInDB = venuesRef.document(venueName);
         venueInDB.update("numberOfAttendees", count);
     }
-    public void updateListOfVenuesAttended(Venue venue){
+    public void addToListOfVenuesAttended(final Venue attendedVenue){
+        //Clicked Venue gets Added to List, updates Counter
+        userRef.get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        User currentUser = documentSnapshot.toObject(User.class);
+                        if (currentUser.getVenuecount() <= 3){
+                            List<Venue> attendedVenuesList = new ArrayList<Venue>();
+                            attendedVenuesList.add(attendedVenue);
+                            currentUser.setVenuesattending(attendedVenuesList);
+                            currentUser.setVenuecount(currentUser.getVenuecount()+1);
+                        }else {
+                            // user selected more than 3 venues for a night
+                            Toast.makeText(getApplicationContext(),"You can go to more than 3 venues per night!", Toast.LENGTH_LONG);
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d(TAG, "onFailure: Could not update list of venues attended" + e.toString());
+            }
+        });
+    }
 
+    public void deleteFromListOfVenuesAttended(final Venue v){
+        //Deletes venue v from list in user, updates Counter
+        userRef.get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                            User currentUser = documentSnapshot.toObject(User.class);
+                            List<Venue> updateList = new ArrayList<>(currentUser.getVenuesattending());
+                            updateList.remove(v);
+                            currentUser.setVenuesattending(updateList);
+                            currentUser.setVenuecount(currentUser.getVenuecount()-1);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d(TAG, "onFailure: Could not update list of venues attended" + e.toString());
+            }
+        });
+    }
+
+    /**UserInfo**/
+
+    //Returns String of ID if user is logged in
+    //null otherwise
+    public String getUserId(){
+        if (user!= null){
+            //User is logged in
+            return user.getUid();
+        }else {
+            //User not logged in
+            return null;
+        }
     }
 }
