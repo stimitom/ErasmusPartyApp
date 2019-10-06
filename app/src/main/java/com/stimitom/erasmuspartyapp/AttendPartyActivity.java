f3150c6385008c332c33a9459994dfd4d19bb57a
tstim <e1551905@student.tuwien.ac.at>
2019-10-06 17:35:17 +0300

    lauter scheiße

--- app/src/main/java/com/stimitom/erasmuspartyapp/AttendPartyActivity.java
+++ app/src/main/java/com/stimitom/erasmuspartyapp/AttendPartyActivity.java
@@ -34,7 +34,7 @@ public class AttendPartyActivity extends AppCompatActivity {
     private TextView venueNumberOfAttendees_TextView;
 
     private Button attendButton;
-    private Boolean clicked = false;
+    private Boolean clicked;
 
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
@@ -61,35 +61,32 @@ public class AttendPartyActivity extends AppCompatActivity {
         attendButton = (Button) findViewById(R.id.attend_button);
         venueNumberOfAttendees_TextView = (TextView) findViewById(R.id.number_of_attendees1);
 
-
-
         final Venue venue = getIntent().getParcelableExtra("clickedVenue");
         final String venue_name = venue.getVenueName();
-
-
         venueRef = db.collection("venues").document(venue_name);
-        getVenueData();
 
-        if (user != null){
+        // Check current user
+        if (user != null) {
             currentUserId = getUserId();
-            Log.d(TAG, "onCreate: user is logged in, userRef defined");
-             userRef = db.collection("users")
+            Log.d(TAG, "onCreate: user is logged in, user ID and userRef defined");
+            userRef = db.collection("users")
                     .document(currentUserId);
-        }else{
+            getVenueData();
+        } else {
             Log.d(TAG, "onCreate: user is not logged in, should be sent to LOGIN");
             //TODO send to login
         }
-        getUserData();
+
 
 
         attendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (usersCurrentVenueCount < 3) {
-                    if (clicked == false) {
+                    if (!clicked) {
                         Log.d(TAG, "onClick: update of Venue and User will be performed");
                         // Update db venueSide
-                        venueRef.update("numberOfAttendees",++venueNumberOfAttendees);
+                        venueRef.update("numberOfAttendees", ++venueNumberOfAttendees);
                         addUserToVenueGuestList(currentUserId);
                         //update db userSide
                         addToUserListOfAttendedVenues(venue);
@@ -108,38 +105,41 @@ public class AttendPartyActivity extends AppCompatActivity {
                         venueNumberOfAttendees_TextView.setText(Integer.toString(venueNumberOfAttendees));
                         clicked = false;
                     }
-                }else{
+                } else {
                     Toast.makeText(context, "Sorry, you can only attend 3 venues per night!", Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
     }
+
     /***************************/
     /**GET DATA FROM DATABASE**/
 
-    /**UserInfo**/
+    /**
+     * UserInfo
+     **/
     //Returns String of ID if user is logged in
     //null otherwise
-    public String getUserId(){
-        if (user!= null){
+    public String getUserId() {
+        if (user != null) {
             //User is logged in
-            Log.e(TAG, "getUserId: "+ user.getUid() );
+            Log.d(TAG, "getUserId: " + user.getUid());
             return user.getUid();
-        }else {
+        } else {
             //User not logged in
             return null;
         }
     }
 
-    public void getUserData(){
+    public void getUserData() {
         userRef.get()
                 .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
                     @Override
                     public void onSuccess(DocumentSnapshot documentSnapshot) {
                         User user = documentSnapshot.toObject(User.class);
                         usersCurrentVenueCount = user.getVenuecount();
-                        if (usersCurrentVenueCount != 0){
+                        if (usersCurrentVenueCount != 0) {
                             usersCurrentAttendedVenuesList.addAll(user.getVenuesattending());
                         }
                     }
@@ -151,25 +151,54 @@ public class AttendPartyActivity extends AppCompatActivity {
         });
     }
 
-    /**Venue Info **/
+    public void setButtonColorAndText() {
 
-    public void getVenueData(){
+        if (venueGuestList != null) {
+            Log.e(TAG, "onCreate: venueGuestList not null");
+            if (venueGuestList.contains(currentUserId)) {
+                Log.e(TAG, "setButtonColorAndText: venue Guest list contains currentuserID");
+                clicked = true;
+                attendButton.setBackgroundColor(Color.RED);
+                attendButton.setText(R.string.dontgo);
+            } else {
+                Log.e(TAG, "setButtonColorAndText: doesnt contain that shit");
+                clicked = false;
+                attendButton.setText(R.string.attend);
+                attendButton.setBackgroundColor(Color.GREEN);
+            }
+        } else {
+            Log.e(TAG, "onCreate: venueGuestList == null");
+            clicked = false;
+            attendButton.setText(R.string.attend);
+            attendButton.setBackgroundColor(Color.GREEN);
+        }
+
+    }
+
+    /**
+     * Venue Info
+     **/
+
+    public void getVenueData() {
         venueRef.get()
                 .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
                     @Override
                     public void onSuccess(DocumentSnapshot documentSnapshot) {
                         Venue venue = documentSnapshot.toObject(Venue.class);
-
+                        venueGuestList = new ArrayList<String>();
+                        if (venue.getGuestList() != null) venueGuestList.addAll(venue.getGuestList());
                         venueName = venue.getVenueName();
                         venueRating = venue.getRating();
                         venueImageId = venue.getImageId();
                         venueNumberOfAttendees = venue.getNumberOfAttendees();
-                        venueGuestList = venue.getGuestList();
 
                         venueName_TextView.setText(venueName);
                         venuePicture_ImageView.setImageResource(venueImageId);
                         venueRating_TextView.setText(venueRating);
                         venueNumberOfAttendees_TextView.setText(Integer.toString(venueNumberOfAttendees));
+
+                        getUserData();
+                        setButtonColorAndText();
                     }
                 }).addOnFailureListener(new OnFailureListener() {
             @Override
@@ -183,22 +212,24 @@ public class AttendPartyActivity extends AppCompatActivity {
     /***************************/
     /** UPDATE DATABASE **/
 
-    /**User Side **/
+    /**
+     * User Side
+     **/
 
-    public void addToUserListOfAttendedVenues(Venue venue){
+    public void addToUserListOfAttendedVenues(Venue venue) {
         usersCurrentAttendedVenuesList.add(venue);
         usersCurrentVenueCount++;
         userRef.update("venuesattending", usersCurrentAttendedVenuesList);
-        userRef.update("venuecount",usersCurrentVenueCount);
+        userRef.update("venuecount", usersCurrentVenueCount);
         Log.d(TAG, "addToUserListOfAttendedVenues: Updated");
     }
 
-    public void deleteFromUserListOfAttendedVenues(Venue venue){
+    public void deleteFromUserListOfAttendedVenues(Venue venue) {
         List<Venue> updatedList = new ArrayList<>();
         updatedList.addAll(usersCurrentAttendedVenuesList);
-        int positionOfVenueToBeDeleted=0;
-        for (int i = 0; i<updatedList.size(); i++){
-            if (updatedList.get(i).getVenueName().equals(venue.getVenueName())){
+        int positionOfVenueToBeDeleted = 0;
+        for (int i = 0; i < updatedList.size(); i++) {
+            if (updatedList.get(i).getVenueName().equals(venue.getVenueName())) {
                 positionOfVenueToBeDeleted = i;
                 // ListSize can at maximum be 3 so break loop with i = 4;
                 i = 4;
@@ -213,15 +244,17 @@ public class AttendPartyActivity extends AppCompatActivity {
         userRef.update("venuecount", usersCurrentVenueCount);
     }
 
-    /** Venue Side **/
+    /**
+     * Venue Side
+     **/
 
-    public void addUserToVenueGuestList(String userId){
+    public void addUserToVenueGuestList(String userId) {
         venueGuestList = new ArrayList<String>();
         venueGuestList.add(userId);
         venueRef.update("GuestList", venueGuestList);
     }
 
-    public void deleteUserFromVenueGuestList(String userId){
+    public void deleteUserFromVenueGuestList(String userId) {
         venueGuestList.remove(userId);
         venueRef.update("GuestList", venueGuestList);
     }
