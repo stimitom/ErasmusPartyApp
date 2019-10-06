610bf28342f472ed124762fdd7f6f9cc24bc3024
tstim <e1551905@student.tuwien.ac.at>
2019-10-07 00:11:48 +0300

    Registration Activity Coded but not used

--- app/src/main/java/com/stimitom/erasmuspartyapp/RegistrationActivity.java
+++ app/src/main/java/com/stimitom/erasmuspartyapp/RegistrationActivity.java
@@ -1,14 +1,112 @@
 package com.stimitom.erasmuspartyapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class RegistrationActivity extends AppCompatActivity {
+    private static final String TAG = "RegistrationActivity";
+    private EditText editTextName;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private Button loginButton;
+    //TODO ADD PROGRESS BAR
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        editTextName = (EditText) findViewById(R.id.username_edit_text);
+        editTextEmail = (EditText) findViewById(R.id.email_edit_text);
+        editTextPassword = (EditText) findViewById(R.id.password_edit_text);
+        loginButton = (Button) findViewById(R.id.login_button);
+        mAuth = FirebaseAuth.getInstance();
+
+        loginButton.setOnClickListener(loginListener);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mAuth.getCurrentUser() != null){
+            //User already signed in
+        }
+    }
+
+    View.OnClickListener loginListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+           registerUser();
+        }
+    };
+
+    private void registerUser(){
+        final String name = editTextName.getText().toString().trim();
+        final String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if(name.isEmpty()){
+            editTextName.setError("username required");
+            editTextName.requestFocus();
+            return;
+        }
+
+        if(email.isEmpty()){
+            editTextEmail.setError("email required");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextEmail.setError("Enter a valid email");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()){
+            editTextPassword.setError("password required");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if (password.length() <6){
+            editTextPassword.setError("password should be at least 6 characters long");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            User user = new User(name, email);
+                            FirebaseFirestore db = FirebaseFirestore.getInstance();
+                            db.collection("users").
+                                    document(FirebaseAuth.getInstance().getCurrentUser()
+                                            .getUid());
+                        }else {
+                            Log.e(TAG, "onComplete: user upload to db was not succesful" + task.getException().getMessage().toString());
+                        }
+                    }
+                });
+
     }
 }
