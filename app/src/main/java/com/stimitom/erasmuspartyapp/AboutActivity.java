64305433b56def659cb5c2594db3fb2c9873915a
tstim <e1551905@student.tuwien.ac.at>
2019-11-06 17:25:11 +0200

    Google place mended, About screen made

--- app/src/main/java/com/stimitom/erasmuspartyapp/AboutActivity.java
+++ app/src/main/java/com/stimitom/erasmuspartyapp/AboutActivity.java
@@ -2,18 +2,41 @@ package com.stimitom.erasmuspartyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
 
+    private TextView icons8Link;
+    private Button buyMeACoffeeButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
+        icons8Link = (TextView) findViewById(R.id.icons8_link_tv);
+        buyMeACoffeeButton = (Button) findViewById(R.id.buy_me_a_coffee_button);
 
+        icons8Link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("http://www.icons8.com");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+        buyMeACoffeeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("http://www.buymeacoffee.com/mkuOCGM");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
     }
-
-
     }
 
